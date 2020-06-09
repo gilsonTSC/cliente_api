8f885b4cbf40ddd7033c389eb3161bffc8a19a0f
Gilson <gilson.costa@dce.ufpb.br>
2020-06-09 20:42:01 -0300

    Criando os repositorios JPA

---
+++ src/main/java/com/gilsontsc/clientes/model/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.gilsontsc.clientes.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gilsontsc.clientes.model.entity.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer>{
+
+}
